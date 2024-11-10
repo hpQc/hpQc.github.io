dc1341c4f95cd08e2267a57ea1784857188da9f4
“hqQc” <qzq7512@gmail.com>
2024-11-10 12:11:02 +0800

    Site updated: 2024-11-10 12:11:02

--- js/fomal.js
+++ js/fomal.js
@@ -285,7 +285,7 @@ function showWelcome() {
   try {
     //自定义文本和需要放的位置
     document.getElementById("welcome-info").innerHTML =
-      `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;
+      `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b><!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "f4cfa77256f14899bba32faaf0949f99"}'></script><!-- End Cloudflare Web Analytics -->`;
   } catch (err) {
     // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
   }
@@ -3473,7 +3473,6 @@ function createWinbox() {
 </div>
 
 </div>
-<!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "f4cfa77256f14899bba32faaf0949f99"}'></script><!-- End Cloudflare Web Analytics -->
 `;
 
   // 打开小窗时候初始化
